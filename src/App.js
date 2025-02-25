ba022dd288c83f92460cd7347699a85a6f67ac6e
AbderrahmenTalbi <a.talbi@esi-sba.dz>
2025-02-25 19:08:53 +0100

    Added Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import SemesterGPACalculator from "./pages/calc";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
-    <div >
+    <div>
+      <SpeedInsights />
       <SemesterGPACalculator />
     </div>
   );
